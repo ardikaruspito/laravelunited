ef8eea6fb53697b5a140da763ddcb8b4e472f11f
unknown <ardikaruspito98@gmail.com>
2019-03-06 10:54:25 +0700

    laravelunited

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Ardika Tri Ruspito</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    
+
+    <link rel="stylesheet" href="{{ ('crossbody/css/open-iconic-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ ('crossbody/css/animate.css') }}">
+    
+    <link rel="stylesheet" href="{{ ('crossbody/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ ('crossbody/css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ ('crossbody/css/magnific-popup.css') }}">
+
+    <link rel="stylesheet" href="{{ ('crossbody/css/aos.css') }}">
+
+    <link rel="stylesheet" href="{{ ('crossbody/css/ionicons.min.css') }}">
+
+    <link rel="stylesheet" href="{{ ('crossbody/css/bootstrap-datepicker.css') }}">
+    <link rel="stylesheet" href="{{ ('crossbody/css/jquery.timepicker.css') }}">
+
+    
+    <link rel="stylesheet" href="{{ ('crossbody/css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ ('crossbody/css/icomoon.css') }}">
+    <link rel="stylesheet" href="{{ ('crossbody/css/style.css') }}">
+  </head>
+  <body>
+  	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="index.html">Ardika Tri Ruspito</a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	        </ul>
+	      </div>
+		  </div>
+	  </nav>
+    <!-- END nav -->
+
+
+    <section class="hero-wrap js-fullheight">
+      <div class="overlay" style="background-image: url(crossbody/images/bg_2.jpg);" data-stellar-background-ratio="0.5"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-end">
+        	<h3 class="v">
+        		<a href="https://vimeo.com/45830194" class="popup-vimeo">
+        			<div class="icon d-flex justify-content-center align-items-center">
+        				<span class="icon-play"></span>
+        			</div>
+        			<span>Crossfit Workout Harder</span> 
+        	</h3>
+          <div class="col-md-12 text-center ftco-animate">
+            <h1 class="mt-5" style="background-image: url(crossbody/images/bg_1.jpg);" data-stellar-background-ratio="0.5">Cross<br>Body</h1>
+            <h2>173140914111010</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+
+  <script src="{{ ('crossbody/js/jquery.min.js') }}"></script>
+  <script src="{{ ('crossbody/js/jquery-migrate-3.0.1.min.js') }}"></script>
+  <script src="{{ ('crossbody/js/popper.min.js') }}"></script>
+  <script src="{{ ('crossbody/js/bootstrap.min.js') }}"></script>
+  <script src="{{ ('crossbody/js/jquery.easing.1.3.js') }}"></script>
+  <script src="{{ ('crossbody/js/jquery.waypoints.min.js') }}"></script>
+  <script src="{{ ('crossbody/js/jquery.stellar.min.js') }}"></script>
+  <script src="{{ ('crossbody/js/owl.carousel.min.js') }}"></script>
+  <script src="{{ ('crossbody/js/jquery.magnific-popup.min.js') }}"></script>
+  <script src="{{ ('crossbody/js/aos.js') }}"></script>
+  <script src="{{ ('crossbody/js/jquery.animateNumber.min.js') }}"></script>
+  <script src="{{ ('crossbody/js/bootstrap-datepicker.js') }}"></script>
+  <script src="{{ ('crossbody/js/jquery.timepicker.min.js') }}"></script>
+  <script src="{{ ('crossbody/js/scrollax.min.js') }}"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="{{ ('crossbody/js/google-map.js') }}"></script>
+  <script src="{{ ('crossbody/js/main.js') }}"></script>
+    
+  </body>
+</html>
